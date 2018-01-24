d93b16950d9002134e2666c70dbab2b31127227b
dxBot <DevExpressExampleBot>
2018-01-24 12:55:57 +0300

    initial commit

---
+++ CS/ComboBoxEdit_CreatingImageComboBox/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace ComboBoxEdit_CreatingImageComboBox {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ CS/ComboBoxEdit_CreatingImageComboBox/Window1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ComboBoxEdit_CreatingImageComboBox {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
