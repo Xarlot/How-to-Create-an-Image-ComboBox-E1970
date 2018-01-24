7dbae7dabb26b3b961db44b8063a9f7d9a0c177b
dxBot <DevExpressExampleBot>
2018-01-24 12:55:58 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Create an Image ComboBox
+
+
+<p>The following example shows how to create an image combobox via templates.</p>
+
+<br/>
+
+
---
+++ VB/ComboBoxEdit_CreatingImageComboBox/Window1.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Windows
+
+Namespace ComboBoxEdit_CreatingImageComboBox
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
